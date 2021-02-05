eb037fd441cb446ae32f4653275b88697deb5425
Adrien <adrien@arccoza.com>
2021-02-05 20:29:56 +0800

    Add undo_redo prop to receive UndoRedo obj from plugin

--- addons/better-texture-array/texture_array_inspector.gd
+++ addons/better-texture-array/texture_array_inspector.gd
@@ -1,9 +1,12 @@
 tool
 extends EditorInspectorPlugin
 
+var undo_redo: UndoRedo
+
 func can_handle(object):
 	return object is Texture3D or object is TextureArray
 
 func parse_property(object, type, path, hint, hint_text, usage):
 	var ed = preload("res://addons/better-texture-array/ui/editor.gd").new()
+	ed.undo_redo = undo_redo
 	add_property_editor("data", ed)
