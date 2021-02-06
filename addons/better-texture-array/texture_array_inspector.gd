cf5503de3b1046967ccd47baf6641139d3fc43a9
Adrien <adrien@arccoza.com>
2021-02-06 23:28:36 +0800

    Fix comment formatting

--- addons/better-texture-array/texture_array_inspector.gd
+++ addons/better-texture-array/texture_array_inspector.gd
@@ -8,8 +8,8 @@ func can_handle(object):
 	return object is Texture3D or object is TextureArray
 
 func parse_property(object, type, path, hint, hint_text, usage):
-	# Use the 'flags' property as our anchor because there is no layers / data prop exported,
-	# make sure not to return 'true' at the end because that would remove the flags UI
+	# Use the `flags` property as our anchor because there is no layers / data prop exported,
+	# make sure not to return `true` at the end because that would remove the flags UI
 	if path != "flags":
 		return
 	
