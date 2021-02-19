af396e95d3c2f04e271911abb8b9d964d2547d49
Adrien <adrien@arccoza.com>
2021-02-19 12:20:49 +0800

    Remove debug print

--- addons/better-texture-array/texture_array_saver.gd
+++ addons/better-texture-array/texture_array_saver.gd
@@ -9,7 +9,6 @@ func recognize(res: Resource) -> bool:
 	return res is TextureArray or res is Texture3D
 
 func save(path: String, res: Resource, flags: int) -> int:
-	prints(path, res, flags)
 	var imp = preload("res://addons/better-texture-array/texture_array_builder.gd").new()
 	imp.is_3d = path.get_extension() == "tex3d"
 	res.take_over_path(path)
